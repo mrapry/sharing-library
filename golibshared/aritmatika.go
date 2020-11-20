b5f2e44c5ed1c0bb6b00844f0269d5605a4886e7
Matius Apry Prastowo <mr.apry.prastowo@gmail.com>
2020-11-20 16:25:35 +0700

    add feature pembagian in arimatika

--- golibshared/aritmatika.go
+++ golibshared/aritmatika.go
@@ -9,3 +9,8 @@ func Penjumlahan(a, b int) int {
 func Pengurangan(a, b int) int {
 	return a - b
 }
+
+//Pembagian function for handling pembagian
+func Pembagian(a, b int) float64 {
+	return float64(a) / float64(b)
+}
